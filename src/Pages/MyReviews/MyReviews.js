dbe255d75f0cbf51c35ffc129769fa99c2cdeca8
nasar06 <nasar5847@gmail.com>
2022-11-09 00:31:28 +0600

    my review section design is done

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../Contexts/AuthProvider';
 
 const MyReviews = () => {
+    const { user } = useContext(UserContext)
+    const [myReview, setMyReview] = useState()
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/myReviews?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setMyReview(data)
+                console.log(data)
+            })
+            .catch(err => console.error(err))
+    }, [])
+
+
+
     return (
         <div>
-            <h1>this is My reviews</h1>
+            <div
+                className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+
+                    <thead>
+                        <tr className='bg-red-500'>
+                            <th>Service Name</th>
+                            <th>Review</th>
+                            <th>status</th>
+                        </tr>
+                    </thead>
+                    {
+                        myReview?.map(myView => < tbody
+                            key={myView?._id}
+                        >
+                            <tr>
+                                <th>{myView?.serviceName}</th>
+                                <td>{myView?.review}</td>
+                                <td>
+                                    <button className="px-3 bg-lime-500 text-white">Edit</button>
+                                    <button className="px-3 btn-error ml-3 text-white">Delete</button>
+                                </td>
+                            </tr>
+                        </tbody >)
+                    }
+                </table>
+            </div>
+
         </div>
     );
 };
